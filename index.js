f4c2b64df94e99e7c9e83eddef0b01f157a0e01f
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-07 15:48:23 +0100

    done

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ whoareyou: 'Fuck you!' });
 });
 
 // listen for requests :)
